18fe494db54571dd5742f830518fe78798b99a7f
MSI\Andy <damh17@gmail.com>
2018-01-01 21:06:51 +0300

    Add update and delete options

--- demo/src/main/java/com/vebinar/controller/UserController.java
+++ demo/src/main/java/com/vebinar/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.vebinar.controller;
 
+import com.vebinar.entity.User;
 import com.vebinar.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
@@ -15,17 +15,58 @@ public class UserController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "index"; // name of *.ftl
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/hello") // context
     public String hello() {
-        return "hello";
+        return "hello"; // name of *.ftl
     }
 
-    @GetMapping("/users")
+    @GetMapping("/users") // context
     public String getAllUsers(Model model) {
         model.addAttribute("users", userService.findAll());
-        return "usersList";
+
+        return "usersList"; // name of *.ftl
+    }
+
+    @GetMapping("/user/{id}") // context
+    public String getById(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getById(id));
+
+        return "showUser"; // name of *.ftl
+    }
+
+    @GetMapping("/addUser")
+    public String createUserPage() {
+        return "createUser";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+
+        return "redirect:/users";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        userService.delete(id);
+
+        return "redirect:/users";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getById(id));
+
+        return "editUser";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.update(user);
+
+        return "redirect:/user/" + user.getId();
     }
 }
--- demo/src/main/java/com/vebinar/dao/UserDao.java
+++ demo/src/main/java/com/vebinar/dao/UserDao.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface UserDao {
     List<User> findAl();
+
+    void save(User user);
+
+    User getById(int id);
+
+    void delete(int id);
+
+    void update(User user);
 }
